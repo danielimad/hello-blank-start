f8329559650bd16a199b6156154f940e4f4d36ef
0xDanielimad <72716286+danielimad@users.noreply.github.com>
2025-08-05 16:04:26 +0400

    Add main App component displaying Hello World

---
+++ src/app.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-50">
+      <h1 className="text-4xl font-bold text-gray-900">Hello World</h1>
+    </div>
+  )
+}
+
+export default App
